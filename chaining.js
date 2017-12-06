8641af867bae3718d574c45eebcf6e40dda51777
kylesaldana5 <kylesaldana5@gmail.com>
2017-12-06 13:01:25 -0600

    replaced func with fat arrow

--- chaining.js
+++ chaining.js
@@ -7,5 +7,5 @@
 
 var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
 
-let numArry = integers.sort(function (a, b) { return b - a }).filter(function (remove) { return remove <= 19 }).map(function (num) { return (num * 1.5 - 1) }).reduce(function(a, b){ return a + b})
+let numArry = integers.sort((a, b) => b - a ).filter((remove) => remove <= 19 ).map((num) => (num * 1.5 - 1)).reduce((a, b)=> a + b)
 console.log(numArry) 
